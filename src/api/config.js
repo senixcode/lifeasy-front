58d06229c0df0baff75ec68aefb157a35ac09716
Luis Romero <codesenix@gmail.com>
2022-09-18 13:38:20 -0500

    add   Environment

--- src/api/config.js
+++ src/api/config.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'http://localhost:3001',
+    baseURL: process.env.API_URL || 'http://localhost:3001',
     timeout: 1000,
 });
 
